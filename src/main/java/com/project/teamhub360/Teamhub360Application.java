0c99ea6acafe85e249054efa5eb93143f57bee51
jorgenano8 <jorgenano8@gmail.com>
2024-03-07 13:36:53 +0100

    first commit

---
+++ src/main/java/com/project/teamhub360/Teamhub360Application.java
@@ -0,0 +1,13 @@
+package com.project.teamhub360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teamhub360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teamhub360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/teamhub360/Teamhub360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.teamhub360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teamhub360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
